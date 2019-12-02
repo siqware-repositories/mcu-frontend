1e44d750385cf543748d432159427cd3c768be69
tunchamroeun <chamroeuntun25@outlook.com>
2019-12-02 08:17:40 +0700

    mcu frontend update api

--- resources/js/src/store/modules/frontend.js
+++ resources/js/src/store/modules/frontend.js
@@ -1,5 +1,6 @@
 import axios from  'axios'
 axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
+axios.defaults.headers.common['Access-Control-Allow-Methods'] = 'GET,PUT,POST,DELETE,OPTIONS';
 const state = {
     corporation:[],
     news:[],
